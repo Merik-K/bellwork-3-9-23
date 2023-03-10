72b21cf6dc032a44bc497d26821ad5a00c783dfa
MerikKuhn <mkuhn181@west-mec.org>
2023-03-09 18:14:51 -0700

    bjcyfgvhbj

---
+++ bellwork.js
@@ -0,0 +1,25 @@
+/*1. Complete the solution so that it reverses the string passed into it.
+For example: 'world'  =>  'dlrow' OR 'word'   =>  'drow'*/
+
+function solution(str) {
+    
+}
+
+/*2. it is important to stay hydrated, he drinks 0.5 litres of water per hour of cycling.
+You get given the time in hours and you need to return the number of litres You will need
+to drink, rounded to the smallest value.*/
+
+function litres(time) {
+  return 0;
+}
+
+/*3. Create a difference function, which subtracts one list from another and returns the result.
+It should remove all values from list a, which are present in list b keeping their order.
+For example:
+arrayDiff([1,2],[1]) == [2]
+
+If a value is present in b, all of its occurrences must be removed from the other:
+For example:
+arrayDiff([1,2,2,2,3],[2]) == [1,3]*/
+
+function arrayDiff(a, b) {}
